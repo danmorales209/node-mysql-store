2d04b28085a11356f6678d6ed2e2552d12d333cd
danmorales209 <danmorales209@gmail.com>
2019-03-11 17:13:28 -0700

    change SQL intermediate queries from connection.end to connection.pause

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,8 +1,8 @@
 console.clear();
 
-require('dotenv').config(); 
+require('dotenv').config();
 var mysql = require('mysql');
-var inquire = require ('inquirer');
+var inquire = require('inquirer');
 var server = require('./myConnection');
 var productTable = require('./util/printTable');
 
@@ -10,25 +10,81 @@ var connection = mysql.createConnection(server);
 
 
 
-var main = function() {
+var main = function () {
     console.clear();
     var myTable = new productTable();
-    showTable(myTable, myTable.initialPrint);
+    showTablePrompt(myTable);
+
+
+
 
 }
 
-var showTable = function(obj) {
+var showTablePrompt = function (obj) {
+
     connection.query("SELECT * FROM products", function (error, data) {
         if (error) {
             return console.error(error);
         }
-        
+
         obj.setData(data);
         obj.initialPrint()
-        
-        connection.end();
+        connection.pause();
+
+        // getUserInput();
+
+        inquire
+            .prompt({
+                type: "list",
+                name: "selection",
+                message: "Welcome to Bamazon! HOw can we help you today?",
+                choices: ["Buy an item", "Quit"]
+            }).then(function (response) {
+
+                if (response.selection ==="Buy an item") {
+                    console.log("Ok, lets buys someting");
+                    connection.resume();
+                    getUserInput();
+                }
+                else {
+                    console.log("Goodbye!");
+                    connection.end();
+                }
+
+
+
+
+            });
+
     });
 
 };
+
+var getUserInput = function () {
+    inquire
+        .prompt([
+            {
+                name: "selectedID",
+                message: "Please select an ID to purchase"
+            },
+            {
+                name: "purchaseAmount",
+                message: "How much would you like to purchase?"
+            }
+        ]).then(function (response) {
+            console.log(response);
+
+            connection.query("SELECT * FROM products", function (err, data) {
+                if (err) throw err;
+
+                console.log(data);
+
+                connection.pause();
+
+            });
+
+        });
+
+}
 
 main();
