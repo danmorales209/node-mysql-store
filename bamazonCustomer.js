b1bfa84cd9e5d58ccb59ab99b253d2c90df4db4b
danmorales209 <danmorales209@gmail.com>
2019-03-13 15:04:53 -0700

    user input functions properly now, add some comments

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,25 +1,27 @@
 console.clear();
+// Define project dependencies
 
+// Use dotenv to store database logins securely
 require('dotenv').config();
 var mysql = require('mysql');
 var inquire = require('inquirer');
 var server = require('./myConnection');
-var productTable = require('./util/printTable');
+var dataTable = require('./util/printTable');
 
+// create the SQL connection as a global variable
 var connection = mysql.createConnection(server);
 
-
-
+// Main starts the program and initializes the productTable object;
 var main = function () {
     console.clear();
-    var myTable = new productTable();
+    var myTable = new dataTable();
     showTablePrompt(myTable);
-
-
-
-
 }
 
+/**
+ * Takes in a dataTable object, and 
+ * @param {dataTable} obj 
+ */
 var showTablePrompt = function (obj) {
 
     connection.query("SELECT * FROM products", function (error, data) {
@@ -30,8 +32,6 @@ var showTablePrompt = function (obj) {
         obj.setData(data);
         obj.initialPrint()
         connection.pause();
-
-        // getUserInput();
 
         inquire
             .prompt({
@@ -54,36 +54,75 @@ var showTablePrompt = function (obj) {
     });
 };
 
+var checkForInt = function (value) {
+    let pattern = new RegExp("^\\d+\\.{0}\\d*$");
+
+    let result = pattern.test(value);
+
+    if (result) {
+        return true;
+    }
+    else {
+        return "Please enter a number";
+    }
+
+};
+
+var checkBuyAmount = function(amount) {
+    let isValidAmount = checkForInt(amount) && Number(amount) > 0;
+
+    if (isValidAmount) {
+        return true;
+    }
+
+    else {
+        return "Please enter a non-negtive integer."
+    }
+};
+
 var getUserInput = function (obj) {
     inquire
         .prompt([
             {
                 name: "selectedID",
-                message: "Please select an ID to purchase"
+                message: "Please select an ID to purchase",
+                validate: checkForInt
             },
             {
                 name: "purchaseAmount",
-                message: "How much would you like to purchase?"
+                message: "How much would you like to purchase?",
+                validate: checkBuyAmount
             }
         ]).then(function (response) {
-            let newValue = obj.data[Number(response.selectedID)-1].stock_quantity - Number(response.purchaseAmount);
+            
+            let newValue = obj.data[Number(response.selectedID) - 1].stock_quantity - Number(response.purchaseAmount);
+            let validIDs = obj.data.map(row => row.item_id);
 
-            if (newValue < 0) {
-                newValue = 0;
+            console.clear();
+            
+            if (validIDs.indexOf(Number(response.selectedID)) < 0) {
+                console.log("I'm sorry, that ID doesn't match anything in our records, please try again.");
+                showTablePrompt(obj);
             }
 
-            connection.query(`UPDATE products SET stock_quantity = ${newValue} WHERE item_id=${Number(response.selectedID)}`, function (err) {
-
-                if (err) {
-                    throw err;
-                }
-                // connection.pause();
-
+            else if (newValue < 0) {
+                console.log(`Sorry, Bamazon only has ${obj.data[Number(response.selectedID) - 1].stock_quantity} of ${obj.data[Number(response.selectedID) - 1].product_name}, your request has been cancelled.`);
                 showTablePrompt(obj);
-            });
+            }
+            else {
 
+                console.log(`Thank you for your purchase! ${response.purchaseAmount} units of ${obj.data[Number(response.selectedID) - 1].product_name} cost you $${(Number(response.purchaseAmount) * obj.data[Number(response.selectedID) - 1].price).toFixed(2)}.`)
+
+                connection.query(`UPDATE products SET stock_quantity = ${newValue} WHERE item_id=${Number(response.selectedID)}`, function (err) {
+
+                    if (err) {
+                        throw err;
+                    }
+
+                    showTablePrompt(obj);
+                });
+            }
         });
-
 }
 
 main();
