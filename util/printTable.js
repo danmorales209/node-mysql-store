5ab10af5dfc9ee33cdd1edf10dbbbd24d08c0551
danmorales209 <danmorales209@gmail.com>
2019-03-20 12:40:32 -0700

    add comments to printTable

--- util/printTable.js
+++ util/printTable.js
@@ -1,6 +1,10 @@
+/**
+ * Constructor for dataTable Class
+ */
 var dataTable = function () {
     this.data = [];
 
+    // Use .width for printing data
     this.width = {
         item_id: 5,
         product_name: 50,
@@ -10,47 +14,64 @@ var dataTable = function () {
     };
 
 
+    /**
+     * Setter for .data
+     */
     this.setData = function (sqlData) {
         this.data = sqlData;
     };
-
+     /**
+      * Getter for .data
+      */
     this.getData = function () {
         return this.data;
     };
 
+    /**
+     * Reset the .data
+     */
     this.reset = function () {
         this.data = [];
     };
 
+    /**
+     * Accepts an array of strings of column headers.
+     * Uses the .width values to build a string of '-' chartacters to print nice breaks
+     */
+
     this.printBreak = function (columnsArray) {
 
-        let breakString = "";
+        let breakString = "-";
 
-
-        breakString += "-";
         columnsArray.forEach((col) => {
             breakString += "-";
+
             switch (col) {
+
                 case "item_id":
                     for (i = 0; i < this.width.item_id + 2; i++) {
                         breakString += "-";
                     }
                     break;
+
                 case "product_name":
                     for (i = 0; i < this.width.product_name + 2; i++) {
                         breakString += "-";
                     }
                     break;
+
                 case "department_name":
                     for (i = 0; i < this.width.department_name + 2; i++) {
                         breakString += "-";
                     }
                     break;
+
                 case "price":
                     for (i = 0; i < this.width.price + 2; i++) {
                         breakString += "-";
                     }
                     break;
+
                 case "stock_quantity":
                     for (i = 0; i < this.width.stock_quantity + 2; i++) {
                         breakString += "-";
@@ -62,31 +83,55 @@ var dataTable = function () {
         console.log(breakString);
     }
 
-    this.printEntry = function (label, padding) {
+    /**
+     * Method formats data for a cell based upon the maxmium width passed into the method
+     * Pads the data with a leading space and sufficient spaces after up to the maxWidth paramater
+     * 
+     * Returns a string
+     * @param {String} data
+     * @param {int} maxWidth
+     */
+    this.printEntry = function (data, maxWidth) {
+        
         let cellString = "";
-        for (let i = 0; i < padding; i++) {
+        
+        for (let i = 0; i < maxWidth; i++) {
             if (i !== 1) {
                 cellString += " ";
             }
             else {
-                cellString += label;
-                i += label.length - 1;
+                cellString += data;
+                // Advance the counter. Use -1 as the for loop with automatically advance i
+                i += data.length - 1;
             }
         }
         return cellString;
     };
 
+    /**
+     * Accepts an array of strings of the SQL data labels to print from .data
+     * 
+     * 
+     * @param {Array} columns
+     */
+
     this.printRows = function (columns) {
+        
         let keys = [];
         let outString = "";
 
         this.data.forEach(row => {
+            
             outString = "| ";
+            // get an array of keys from each row object
             keys = Object.keys(row);
 
             for (i in keys) {
-                outString += this.printEntry((row[keys[i]]).toString(), this.width[keys[i]]);
 
+                // keys[i] gets the data in the row, and width [ keys [i] ] gets the max width from the object
+                outString += this.printEntry( (row[ keys[i] ]).toString() , this.width[ keys [i] ] );
+
+                // i is stored as a string for some reason
                 if (keys[Number(i) + 1]) {
                     outString += " | ";
                 }
@@ -95,12 +140,19 @@ var dataTable = function () {
                 }
 
             }
+
             console.log(outString);
+
+            // Prints "-" character break of same width as data
             this.printBreak(columns);
 
         }, this);
     }
 
+    /**
+     * Accepts a string array of SQL data types to prints headers for the table
+     * @param {Array} strArray
+     */
     this.printHeaders = function (strArray) {
         let padding = 0;
         let outString = "| ";
@@ -191,11 +243,15 @@ var dataTable = function () {
         console.log(outString);
     };
 
-    this.initialPrint = function (headerArray = ["item_id", "product_name", "department_name", "price", "stock_quantity"]) {
-        this.printBreak(headerArray);
-        this.printHeaders(headerArray);
-        this.printBreak(headerArray);
-        this.printRows(headerArray);
+    /**
+     * Accepts an array of strings with the data labels for the SQL column headers to print a formatted table
+     * @param {Array} columns
+     */
+    this.initialPrint = function (columns = ["item_id", "product_name", "department_name", "price", "stock_quantity"]) {
+        this.printBreak(columns);
+        this.printHeaders(columns);
+        this.printBreak(columns);
+        this.printRows(columns);
     };
 
 };
